bf90e5568c0a6879504faa385927ee218f4f7f51
achrefbenabdallah <achref.ben-abdallah@tek-up.tn>
2021-05-16 14:39:15 +0100

    add Vue request.html and package Controller

---
+++ src/main/java/com/example/demo/controllers/CheckController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.student.service.SoapStudent;
+import com.example.demo.whitetest.StudentRequest;
+import com.example.demo.whitetest.WhiteTestResponse;
+
+@Controller
+public class CheckController {
+	
+	@Autowired
+	private SoapStudent student;
+	
+	@GetMapping("/check/whitetest")
+	public String CheckWhiteTestForm(Model model) {
+		StudentRequest studentRequest = new StudentRequest();
+		model.addAttribute("request", studentRequest);
+		return"request";
+	}
+	@PostMapping("/check/whitetest")
+	public String CheckResults(@ModelAttribute("request") StudentRequest request ,Model model) {
+		WhiteTestResponse response = student.getWhiteTestStatus(request);
+		model.addAttribute("response", response);
+		return "results";
+	}
+
+}
