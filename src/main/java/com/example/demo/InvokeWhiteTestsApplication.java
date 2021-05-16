782d426807fb2a4735b38ed3713e9f1ca22079ef
achrefbenabdallah <achref.ben-abdallah@tek-up.tn>
2021-05-16 11:32:25 +0100

    add WhiteTest description wsdl

---
+++ src/main/java/com/example/demo/InvokeWhiteTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvokeWhiteTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvokeWhiteTestsApplication.class, args);
+	}
+
+}
